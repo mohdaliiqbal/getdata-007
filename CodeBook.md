4b778ac0de9164bc5134132e10edcfe686c51517
mohdaliiqbal <ali.iqbal@gmail.com>
2014-09-22 01:56:21 +0400

    Finalized the run_analysis.R script and code book

--- CodeBook.md
+++ CodeBook.md
@@ -1,1 +1,52 @@
-s
+#Requirements
+#   You should create one R script called run_analysis.R that does the following. 
+#1. Merges the training and the test sets to create one data set.
+#2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+#3. Uses descriptive activity names to name the activities in the data set
+#4. Appropriately labels the data set with descriptive variable names. 
+#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+The data from the following URL should be unzipped in this directory.
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+So the folder structure of the current directory should look something like
+/test (directory with all test data from the original zip file)
+/train (directory with all training data from the original zip file)
+activity_labels.txt
+features.txt
+features_info.txt
+README.txt
+run_analysis.R 
+
+Variables
+- feature - Names of the features in the  feature.txt file. The data is cleaned up to remove the feature number in the file
+- data_train - Raw data from the training set X_train.txt containing all the features.
+- activity_train - The activity number as per provided in the Y_train.txt
+- data_test - Raw data from the test set X_test.txt containing all the features.
+- activity_test - The activity number as per provided in the Y_test.txt
+
+Transformation steps
+1. Features names are applied first to the training and test data this will give us appropriate labels to the raw data
+
+2. We apply label (activity_id) to the activity data as well
+
+3. We merge the training and test data sets with its respective activity data using cbind
+
+4. We merge two data sets (training, and test) using rbind
+
+5. We grep the columns we are interested in i.e. mean, standard, activity id and store it in intermediate variable mean_std_measurements
+
+6. We load the activity labels data
+
+7. We merge the activity labels data with the data in the step 5.
+
+8. We discard the activity_id column from the merged data
+
+9. We output the data using write.table and we ask it not to write the row numbers. We use CSV format
+
+
+tidy data set contains, all mean, std measurements from all the measurements and also the name of the activity
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -6,32 +6,62 @@
 #4. Appropriately labels the data set with descriptive variable names. 
 #5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-#read the training file
-data_train<-read.table("./train/X_train.txt")
-data_test<-read.table("./test/X_test.txt")
-
-#merge the data
-#Fulfil Requirement 1. Merges the training and the test sets to create one data set.
-merged_data<-rbind(data_train,data_test)
-
-
 #read the features name to label
 features<-read.table("features.txt")
 
 #extract only feature name column
 features<-as.character(features[,2])
 
-#now put the names to the columns/features over the data
-names(merged_data) <- features
 
+#read the training data and training activity files data
+data_train<-read.table("./train/X_train.txt")
+activity_train<-read.table("./train/Y_train.txt")
+
+#renaming the activity data and putting a column name activity_id
+names(activity_train)<-c("activity_id")
+
+#now put the names to the columns/features over the data
+names(data_train) <- features
+
+#merge the training data and activity
+merged_train_data<- cbind(data_train,activity_train)
+
+
+#load testing data and activity names
+data_test<-read.table("./test/X_test.txt")
+activity_test<-read.table("./test/Y_test.txt")
+
+#renaming the activity data and putting a column name activity_id
+names(activity_test)<-c("activity_id")
+names(data_test) <- features
+
+#merge the training data and activity
+merged_test_data<- cbind(data_test,activity_test)
+
+#merge the data
+#Fulfil Requirement 1. Merges the training and the test sets to create one data set.
+merged_data<-rbind(merged_train_data,merged_test_data)
 
 #now we will subset the data and extract only columns which are mean or std deviations
 #following line will extract all the columns which have either mean or std in their names. So that 
 
 #Fulfil Requirement 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-mean_std_measurements<-merged_data[,grep("mean\\(|std",colnames(merged_data))]
+mean_std_measurements<-merged_data[,grep("mean\\(|std|activity_id",colnames(merged_data))]
 
+#lets apply the label to the data.
+#load the activity labels	
+activity_label<-read.table("activity_labels.txt")
 
+#give a proper name to the columns in the activity_label
+names(activity_label)<-c("activity_id","activity_name")
 
+#now activity_id column can be used to merge the mean_std_measurements and activity_label to put descriptive label to the activity
+labeled_data_set<-merge(mean_std_measurements,activity_label,x.by="activity_id",y.by="activity_id")
+
+#remove the activity_id column as its not needed in the tidy data set
+final_tidy_data_set<-labeled_data_set[,-1] 
+
+#write the data to the tidy data set file.
+write.table(tidy_data_set,"tidy_data.txt",row.names=FALSE)
 
 
